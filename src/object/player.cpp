9b07c100bbe53175c41568702eff01fec8557e4e
headshot2017 <nou@jm>
2022-12-25 17:34:13 -0400

    grow tux if mario spawns (alter hitbox)

--- src/object/player.cpp
+++ src/object/player.cpp
@@ -2026,7 +2026,10 @@ Player::set_pos(const Vector& pos, bool tuxToMario)
   if (m_mario && !tuxToMario)
   {
     if (!m_mario_obj->spawned())
+    {
       m_mario_obj->spawn(get_pos().x, get_pos().y);
+      add_bonus(GROWUP_BONUS, false);
+    }
     else
       m_mario_obj->set_pos(pos);
   }
