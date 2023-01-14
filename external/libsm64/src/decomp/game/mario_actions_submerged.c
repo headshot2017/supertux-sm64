05e08247f5ca620500351d3bb42e574dfb62039d
headshot2017 <nou@jm>
2023-01-13 21:21:38 -0400

    libsm64: tweak underwater hitbox

--- external/libsm64/src/decomp/game/mario_actions_submerged.c
+++ external/libsm64/src/decomp/game/mario_actions_submerged.c
@@ -86,7 +86,7 @@ static u32 perform_water_full_step(struct MarioState *m, Vec3f nextPos) {
     f32 ceilHeight;
     f32 floorHeight;
 
-    wall = resolve_and_return_wall_collisions(nextPos, 10.0f, 110.0f);
+    wall = resolve_and_return_wall_collisions(nextPos, 10.0f, 60.0f);
     floorHeight = find_floor(nextPos[0], nextPos[1], nextPos[2], &floor);
     ceilHeight = vec3f_find_ceil(nextPos, floorHeight, &ceil);
 
