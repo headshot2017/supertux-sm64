1fd24d473f6b477ba0e61c6c2a1e76f2bdd4dca8
headshot2017 <nou@jm>
2023-02-05 02:59:56 -0400

    fix intro cutscene

--- src/object/player.cpp
+++ src/object/player.cpp
@@ -2200,7 +2200,7 @@ Player::set_pos(const Vector& pos, bool tuxToMario)
 {
   m_col.set_pos(pos);
 
-  if (m_mario && m_mario_obj->spawned() && !tuxToMario)
+  if (m_mario && m_mario_obj->spawned() && !tuxToMario && !m_deactivated)
     m_mario_obj->set_pos(Vector(get_pos().x + m_col.m_bbox.get_width()/2 - 2, get_pos().y+40));
 }
 
